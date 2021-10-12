c678eab1540279883d07e19c34eddb402cbe5ce8
Gowthami2601 <66249633+Gowthami2601@users.noreply.github.com>
2021-10-12 13:58:11 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using BankApplication.Models;
+using BankApplication.Services;
+namespace BankCon
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***********Welcome*************");
+            int x = 0;
+            while (x != 1)
+            {
+                UserChoices userChoice = (UserChoices)Enum.Parse(typeof(UserChoices), Console.ReadLine());
+                switch (userChoice)
+                {
+                    case UserChoices.CreateAccount:
+                        {
+                            BankServices.AddAccount( );
+                            break;
+                        }
+                    case UserChoices.Deposit:
+                        {
+                            BankServices.Deposit();
+                            break;
+                        }
+                    case UserChoices.Withdraw:
+                        {
+                            BankServices.Withdraw();
+                            break;
+                        }
+                    case UserChoices.Transfer:
+                        {
+                            BankServices.Transfer();
+                            break;
+                        }
+                    case UserChoices.Transactions:
+                        {
+                            BankMessages.UserOutput("Enter AccountId");
+                            int actId = BankMessages.GetIntInput();
+                            BankMessages.UserOutput("Enter pin");
+                            string pin = BankMessages.GetStringInput();
+                            TransactionServices.PrintTransaction(actId,pin);
+                            break;
+                        }
+         
+                }
+            }
+        }
+            public enum UserChoices
+        {
+            CreateAccount = 1,
+            Deposit = 2,
+            Withdraw = 3,
+            Transfer = 4,
+            Transactions = 5
+            //
+        }
+
+        }
+    }
