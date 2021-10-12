6863a205abe2ebc975149bbf0303bbcc6d8f40a2
Gowthami2601 <66249633+Gowthami2601@users.noreply.github.com>
2021-10-12 13:57:08 +0530

    Add files via upload

---
+++ Account.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApplication.Models
+{
+    public class Account
+    {
+        private static int applicationNo =1000;
+        public int accountID { get; set; }
+        public string name { get; set; }
+        public double amount { get; set; }
+        public string pin { get; set; }
+
+
+        public Account(string name, string pin)
+        {
+            this.accountID = ++applicationNo;
+            this.pin = pin;
+            this.name = name;
+            this.amount = 0;
+        }
+
+    }
+}
---
+++ Transaction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankApplication.Models
+{
+    public class Transaction
+    {
+        public int fromID { get; set; }
+        public int toId { get; set; }
+        public string msg { get; set; }
+        public double amount { get; set; }
+
+        public Transaction(int fromID, int toId, string msg, double amount)
+        {
+            this.fromID = fromID;
+            this.toId = toId;
+            this.msg = msg;
+            this.amount = amount;
+        }
+    }
+}
